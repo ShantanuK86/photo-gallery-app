066628552bd109305af541e48a79fdb734e6082e
ShantanuK86 <sk9411011994@gmail.com>
2024-03-05 18:43:10 +0530

    new project commit

---
+++ src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+    {
+      "name": "Photo 1",
+      "url": "/images/ig1.jpg",
+      "description": "Photo 1 description",
+      "date": "2020-01-01",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 2",
+      "url": "/images/ig2.jpg",
+      "description": "Photo 2 description",
+      "date": "2020-01-02",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 3",
+      "url": "/images/ig3.jpg",
+      "description": "Photo 3 description",
+      "date": "2020-01-03",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 4",
+      "url": "/images/ig4.jpg",
+      "description": "Photo 4 description",
+      "date": "2020-01-04",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 5",
+      "url": "/images/ig5.jpg",
+      "description": "Photo 5 description",
+      "date": "2020-01-05",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 6",
+      "url": "/images/ig6.jpg",
+      "description": "Photo 6 description",
+      "date": "2020-01-06",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 7",
+      "url": "/images/ig7.jpg",
+      "description": "Photo 7 description",
+      "date": "2020-01-07",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 8",
+      "url": "/images/ig8.jpg",
+      "description": "Photo 8 description",
+      "date": "2020-01-08",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 9",
+      "url": "/images/ig9.jpg",
+      "description": "Photo 9 description",
+      "date": "2020-01-09",
+      "type": "image/jpg",
+    }
+  ]
